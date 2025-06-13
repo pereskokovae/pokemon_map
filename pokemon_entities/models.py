5bc045517ac290d410d1cacf44496c6cd96223ba
pereskokovae <pereskokovae48@gmail.com>
2025-06-13 13:26:24 +0700

    show pokemons on mainpage.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,16 +3,16 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(upload_to='pokemons', blank=True, null=True)
 
     def __str__(self):
         return f'{self.title}'
     
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField()
-    lon = models.FloatField()
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, default=1)
+    lat = models.FloatField(blank=True, null=True)
+    lon = models.FloatField(blank=True, null=True)
     appeared_at = models.DateTimeField(default='')
     disappeared_at = models.DateTimeField(default='')
     level = models.IntegerField(null=True)
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,6 +1,7 @@
 import folium
 import json
 
+from .models import Pokemon
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
@@ -40,12 +41,19 @@ def show_all_pokemons(request):
             )
 
     pokemons_on_page = []
+    pokemons = Pokemon.objects.all()
     for pokemon in pokemons:
+        if pokemon.image:
+            img_url = pokemon.image.url
+        else:
+            img_url = None
+
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': img_url,
+            'title_ru': pokemon.title
         })
+
 
     return render(request, 'mainpage.html', context={
         'map': folium_map._repr_html_(),
