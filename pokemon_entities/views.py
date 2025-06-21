f55e3bfc287ea375d29d824925d1510769e0a0dc
pereskokovae <pereskokovae48@gmail.com>
2025-06-21 16:13:44 +0700

    сorrected a potential bug

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -31,7 +31,10 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 def show_all_pokemons(request):
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    pokemons_entity = PokemonEntity.objects.filter(appeared_at__lt=timezone.localtime(), disappeared_at__gt=timezone.localtime())
+    pokemons_entity = PokemonEntity.objects.filter(
+        appeared_at__lt=timezone.localtime(),
+        disappeared_at__gt=timezone.localtime()
+        )
     for pokemon in pokemons_entity:
         add_pokemon(
             folium_map, pokemon.lat,
@@ -75,37 +78,6 @@ def show_pokemon(request, pokemon_id):
             entity.lon,
             request.build_absolute_uri(entity.pokemon.image.url)
             )
-       
-    previous_evolution = {}
-    next_evolution = {}
-    if pokemon.previous_evolution:
-        title_previous_evolution = pokemon.previous_evolution.title
-        id_previous_evolution = pokemon.previous_evolution.id
-        image_url_previous_evolution = request.build_absolute_uri(pokemon.previous_evolution.image.url)
-          
-        evolution = pokemon.previous_evolution.next_evolutions.first()
-        title_next_evolution = evolution.title
-        id_next_evolution = evolution.id
-        image_url_next_evolution = request.build_absolute_uri(evolution.image.url)
-    else:
-        title_previous_evolution = None
-        id_previous_evolution = None
-        image_url_previous_evolution = None
-        title_next_evolution = None
-        id_next_evolution = None
-        image_url_next_evolution = None
-
-    previous_evolution.update({
-        "title_ru": title_previous_evolution,
-        "pokemon_id": id_previous_evolution,
-        "img_url": image_url_previous_evolution
-        })
-
-    next_evolution.update({
-        "title_ru": title_next_evolution,
-        "pokemon_id": id_next_evolution,
-        "img_url": image_url_next_evolution
-        })
 
     pokemon = {
         "pokemon_id": pokemon.id,
@@ -114,8 +86,8 @@ def show_pokemon(request, pokemon_id):
         "title_jp": pokemon.title_jp,
         "description": pokemon.description,
         "img_url": request.build_absolute_uri(pokemon.image.url),
-        "previous_evolution": previous_evolution,
-        "next_evolution": next_evolution
+        "previous_evolution": pokemon.previous_evolution,
+        "next_evolution": pokemon.next_evolutions.first()
         }
    
     return render(request, 'pokemon.html', context={
